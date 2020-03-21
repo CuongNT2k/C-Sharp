8ae3bc3375c0cd1b9f0f6aafe738f838f28aae45
CuongNT2k <55753481+CuongNT2k@users.noreply.github.com>
2020-03-21 18:14:20 +0700

    Create bai3.cs

---
+++ Buoi2/bai3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi2
+{
+    class bai3
+    {
+        static string[] str;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mhap chuoi : ");
+            string s = Console.ReadLine();
+            Split(s);
+            ht();
+            Console.ReadKey();
+        }
+        static void Split(string s)
+        {
+            str = s.Split(' ');
+            Console.Write("str[]= ");
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.Write(str[i] + ",");
+            }
+        }
+        static void ht()
+        {
+            Console.WriteLine("\nHo: " + str[0]);
+            Console.WriteLine("Ten Dem: " + str[1]);
+            Console.WriteLine("Ten: " + str[2]);
+            Console.WriteLine("nam Sinh: " + str[3]);
+            int x = DateTime.Now.Year - int.Parse(str[str.Length - 1]);
+            Console.WriteLine("Tuoi: {0}", x);
+        }
+    }
+}

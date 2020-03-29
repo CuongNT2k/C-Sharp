5a9ce495e13ca30b7fec6a50838db43ab0556e47
CuongNT2k <55753481+CuongNT2k@users.noreply.github.com>
2020-03-29 15:37:45 +0700

    Create Taisan.cs

---
+++ Buoi3/Bai3/Taisan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi3
+{
+    class Taisan
+    {
+        private string tenTs;
+        private int soLuong;
+        private string tinhTrang;
+
+        public string TenTs { get => tenTs; set => tenTs = value; }
+        public int SoLuong { get => soLuong; set => soLuong = value; }
+        public string TinhTrang { get => tinhTrang; set => tinhTrang = value; }
+
+        public Taisan()
+        {
+
+        }
+
+        public void Nhap()
+        {
+            Console.WriteLine("\t\tNhap thong tin phong\t\t");
+            Console.WriteLine("Ten tai san: ");
+            TenTs = Console.ReadLine();
+            Console.WriteLine("So luong : ");
+            SoLuong = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tinh trang tai san: ");
+            TinhTrang = Console.ReadLine();
+        }
+        
+        public void Xuat()
+        {
+            Console.WriteLine("\t\tTen: "+TenTs+"\t\tSo Luong: "+SoLuong+"\t\tTinh trang: "+TinhTrang);
+        }
+
+        public static int Tong(Taisan taisan, int n)
+        {
+            return taisan.SoLuong * n;
+        }
+    }
+}

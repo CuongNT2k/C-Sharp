810e6c96e04cb8f6998c0195163e310099d4634d
CuongNT2k <55753481+CuongNT2k@users.noreply.github.com>
2020-03-29 15:37:47 +0700

    Create Phong.cs

---
+++ Buoi3/Bai3/Phong.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi3
+{
+    class Phong
+    {
+        private string tenPhong;
+        private string maPhong;
+        private string truongPhong;
+
+        public string TenPhong { get => tenPhong; set => tenPhong = value; }
+        public string MaPhong { get => maPhong; set => maPhong = value; }
+        public string TruongPhong { get => truongPhong; set => truongPhong = value; }
+
+        public Phong()
+        {
+
+        }
+
+        public void Nhap()
+        {
+            Console.WriteLine("\t\tNhap thong tin phong\t\t");
+            Console.WriteLine("Ten phong: ");
+            TenPhong = Console.ReadLine();
+            Console.WriteLine("Ma phong: ");
+            MaPhong = Console.ReadLine();
+            Console.WriteLine("Truong phong: ");
+            TruongPhong = Console.ReadLine();
+        }
+
+        public void Xuat()
+        {
+            Console.WriteLine("\t\tTen phong: "+TenPhong+"\t\tMa phong: "+MaPhong+"\t\tTruong phong: "+TruongPhong);
+        }
+    }
+}

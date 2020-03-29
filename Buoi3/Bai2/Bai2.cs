ca8fc9e90fb8117acc10737ebe638bf3fe30bb7c
CuongNT2k <55753481+CuongNT2k@users.noreply.github.com>
2020-03-29 15:37:56 +0700

    Create Bai2.cs

---
+++ Buoi3/Bai2/Bai2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Buoi3
+{
+    class Bai2
+    {
+        static void Main(string[] args)
+        {
+            Ttb2 a = new Ttb2(1, 1, 1);
+            Ttb2 b = new Ttb2(2, 2, 2);
+            Ttb2 Tong = a + b;
+            Ttb2 Hieu = a - b;
+            a--;
+            Console.WriteLine("Phuong trinh doi dau: ");
+            a.Xuat();
+            Console.WriteLine("Tong 2 phuong trinh: ");
+            Tong.Xuat();
+            Console.WriteLine("Hieu 2 phuong trinh: ");
+            Hieu.Xuat();
+
+            Console.ReadKey();
+        }
+    }
+}

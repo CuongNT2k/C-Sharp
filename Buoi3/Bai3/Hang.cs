2feb18c1b1f04adad6af6c19a0d876e0bf12aa23
CuongNT2k <55753481+CuongNT2k@users.noreply.github.com>
2020-03-29 15:37:52 +0700

    Create Hang.cs

---
+++ Buoi3/Bai3/Hang.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi3
+{
+    class Hang
+    {
+        public static void Main(string[] args)
+        {
+            Phieu phieu = new Phieu();
+            phieu.nhap();
+            Phong phong = new Phong();
+            phong.Nhap();
+            Console.WriteLine("Nhap so luong tai san: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Taisan> taisans = new List<Taisan>(n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhap thong tin tai san thu " + (i + 1));
+                Taisan taisan = new Taisan();
+                taisan.Nhap();
+                taisans.Add(taisan);
+            }
+            phieu.xuat();
+            phong.Xuat();
+            foreach(Taisan x in taisans)
+            {
+                x.Xuat();
+            }
+            Console.WriteLine("Tong so luong: ");
+            Console.ReadKey();
+        }
+    }
+}

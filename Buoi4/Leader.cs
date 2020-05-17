b4cb7f96b04f81547b41f64f70c9ccfd8a2c6af2
CuongNT2k <55753481+CuongNT2k@users.noreply.github.com>
2020-05-17 15:12:55 +0700

    Create Leader.cs

---
+++ Buoi4/Leader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi4
+{
+    class Leader : SinhVienHaUI
+    {
+        private string ChucVu;
+        public Leader()
+        {
+            
+        }
+
+        public string ChucVu1 { get => ChucVu; set => ChucVu = value; }
+
+        public void nhap()
+        {
+            base.Nhap();
+            Console.Write("Nhap chuc vu: ");
+            this.ChucVu1 = Console.ReadLine();
+        }
+        public void xuat()
+        {
+            base.Xuat();
+            Console.WriteLine("Chuc vu: " + this.ChucVu1 + "\n");
+        }
+    }
+}

7229d1ff69098be2912b34daaf20639398c67d70
CuongNT2k <55753481+CuongNT2k@users.noreply.github.com>
2020-05-17 15:12:53 +0700

    Create HocVien.cs

---
+++ Buoi4/HocVien.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi4
+{
+    class HocVien : SinhVienHaUI
+    {
+        private int congNo;
+        public int CongNo { get => congNo; set => congNo = value; }
+        public HocVien()
+        {
+            this.CongNo = 10000;
+        }
+        public void nhap()
+        {
+            base.Nhap();
+        }
+        public void xuat()
+        {
+            base.Xuat();
+            Console.WriteLine("Cong no: "+ this.congNo+"\n");
+        }
+        public void thanhToanCn( int TienTT)
+        {
+            this.congNo -= TienTT;
+        }
+    }
+}

8b8a60f05a0c84d73f17bd1e06e39acdaa68a107
CuongNT2k <55753481+CuongNT2k@users.noreply.github.com>
2020-05-17 15:12:59 +0700

    Create SinhVienHaUI.cs

---
+++ Buoi4/SinhVienHaUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi4
+{
+    class SinhVienHaUI
+    {
+        private string HoTen;
+        private string GioiTinh;
+        private string QueQuan;
+        private string TTYD;
+
+        public string HoTen1 { get => HoTen; set => HoTen = value; }
+        public string GioiTinh1 { get => GioiTinh; set => GioiTinh = value; }
+        public string QueQuan1 { get => QueQuan; set => QueQuan = value; }
+        public string TTYD1 { get => TTYD; set => TTYD = value; }
+
+        public SinhVienHaUI()
+        {
+
+        }
+
+        public void Nhap()
+        {
+            Console.WriteLine("Nhap Ho Ten Sinh Vien: "); this.HoTen = Console.ReadLine();
+            Console.WriteLine("Nhap Gioi Tinh Vien: "); this.GioiTinh = Console.ReadLine();
+            Console.WriteLine("Nhap Que Quan Sinh Vien: "); this.QueQuan = Console.ReadLine();
+            Console.WriteLine("Nhap TTYD cua Sinh Vien(Yes/No): "); this.TTYD = Console.ReadLine();
+        }
+        public void Xuat()
+        {
+            Console.Write("Ho ten: " + this.HoTen + ", Gioi tinh: " + this.GioiTinh + ", Que quan: " + this.QueQuan + ", Tinh trang hon nhan: " + this.TTYD + ", ");
+        }
+    }
+
+}
